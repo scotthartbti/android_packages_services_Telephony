SlimRoms/packages_services_Telephony

Telephony: Pulse Torch On Incoming Call (1/4)
Add preference and make the magic
	happen for ringing

Change-Id: Iffe2ad49af0377f2a1bb9455bfa8ff7141488dd7

src/com/android/phone/Ringer.java
@@ -18,6 +18,7 @@ package com.android.phone;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.media.AudioManager;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
@@ -31,11 +32,14 @@ import android.os.ServiceManager;
 import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.SystemVibrator;
+import android.os.UserHandle;
 import android.os.Vibrator;
 import android.provider.Settings;
 import android.util.Log;
 
+import com.android.internal.util.slim.DeviceUtils;
 import com.android.internal.util.slim.QuietHoursHelper;
+import com.android.internal.util.slim.TorchConstants;
 import com.android.internal.telephony.Phone;
 /**
  * Ringer manager for the Phone app.
@@ -73,6 +77,7 @@ public class Ringer {
     private long mFirstRingStartTime = -1;
     private int mRingerVolumeSetting = -1;
     private int mRingIncreaseInterval;
+    private boolean mWeStartedTorch;
 
     /**
      * Initialize the singleton Ringer instance.
@@ -176,9 +181,27 @@ public class Ringer {
                 mVibratorThread.start();
             }
 
+            final boolean inQuietHours = QuietHoursHelper.inQuietHours(
+                    mContext, Settings.System.QUIET_HOURS_RINGER);
+
+            if (Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.TORCH_WHILE_RINGING, 0) == 1
+                    && DeviceUtils.deviceSupportsTorch(mContext)
+                    && !inQuietHours) {
+                mWeStartedTorch = true;
+                Intent intent = new Intent(TorchConstants.ACTION_ON);
+                intent.putExtra(TorchConstants.STROBE_MODE, true);
+                intent.putExtra(TorchConstants.STROBE_PERIOD,
+                        Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.TORCH_WHILE_RINGING_PERIOD, 500));
+                intent.putExtra(TorchConstants.NOTIFICATION_SHOWN, false);
+                mContext.sendBroadcastAsUser(
+                        intent, new UserHandle(UserHandle.USER_CURRENT));
+            }
+
             int ringerVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_RING);
             if (ringerVolume == 0 && mRingerVolumeSetting <= 0
-                || QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_RINGER)) {
+                || inQuietHours) {
                 if (DBG) log("skipping ring because volume is zero");
                 return;
             }
@@ -257,6 +280,13 @@ public class Ringer {
                 mPowerManager.setAttentionLight(false, 0x00000000);
             } catch (RemoteException ex) {
                 // the other end of this binder call is in the system process.
+            }
+
+            if (mWeStartedTorch) {
+                mWeStartedTorch = false;
+                Intent intent = new Intent(TorchConstants.ACTION_OFF);
+                mContext.sendBroadcastAsUser(
+                        intent, new UserHandle(UserHandle.USER_CURRENT));
             }
 
             if (mHandler != null) {

scotthartbti/android_packages_services_Telephony

Signed-off-by: scott <scott@ubuntu>
Conflicts:
	res/values/cm_strings.xml
	res/xml/call_feature_setting.xml
	src/com/android/phone/Ringer.java

src/com/android/phone/Ringer.java
@@ -18,6 +18,7 @@ package com.android.phone;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.media.AudioManager;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
@@ -31,11 +32,14 @@ import android.os.ServiceManager;
 import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.SystemVibrator;
+import android.os.UserHandle;
 import android.os.Vibrator;
 import android.provider.Settings;
 import android.util.Log;
 
+import com.android.internal.util.beanstalk.DeviceUtils;
 import com.android.internal.util.beanstalk.QuietHoursHelper;
+import com.android.internal.util.beanstalk.TorchConstants;
 import com.android.internal.telephony.Phone;
 /**
  * Ringer manager for the Phone app.
@@ -73,6 +77,7 @@ public class Ringer {
     private long mFirstRingStartTime = -1;
     private int mRingerVolumeSetting = -1;
     private int mRingIncreaseInterval;
+    private boolean mWeStartedTorch;
 
     /**
      * Initialize the singleton Ringer instance.
@@ -176,9 +181,27 @@ public class Ringer {
                 mVibratorThread.start();
             }
 
+            final boolean inQuietHours = QuietHoursHelper.inQuietHours(
+                    mContext, Settings.System.QUIET_HOURS_RINGER);
+
+            if (Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.TORCH_WHILE_RINGING, 0) == 1
+                    && DeviceUtils.deviceSupportsTorch(mContext)
+                    && !inQuietHours) {
+                mWeStartedTorch = true;
+                Intent intent = new Intent(TorchConstants.ACTION_ON);
+                intent.putExtra(TorchConstants.STROBE_MODE, true);
+                intent.putExtra(TorchConstants.STROBE_PERIOD,
+                        Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.TORCH_WHILE_RINGING_PERIOD, 500));
+                intent.putExtra(TorchConstants.NOTIFICATION_SHOWN, false);
+                mContext.sendBroadcastAsUser(
+                        intent, new UserHandle(UserHandle.USER_CURRENT));
+            }
+
             int ringerVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_RING);
             if (ringerVolume == 0 && mRingerVolumeSetting <= 0
-                || QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_RINGER)) {
+                || inQuietHours) {
                 if (DBG) log("skipping ring because volume is zero");
                 return;
             }
@@ -257,6 +280,13 @@ public class Ringer {
                 mPowerManager.setAttentionLight(false, 0x00000000);
             } catch (RemoteException ex) {
                 // the other end of this binder call is in the system process.
+            }
+
+            if (mWeStartedTorch) {
+                mWeStartedTorch = false;
+                Intent intent = new Intent(TorchConstants.ACTION_OFF);
+                mContext.sendBroadcastAsUser(
+                        intent, new UserHandle(UserHandle.USER_CURRENT));
             }
 
             if (mHandler != null) {